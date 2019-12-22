9a3b268ea0b2ebb3edc1fec12fcc59c2a879df79
lbmaian <maian330@gmail.com>
2019-12-21 21:48:38 -0800

    Comment out HarmonyExtensions

--- LbmLib/Harmony/HarmonyExtensions.cs
+++ LbmLib/Harmony/HarmonyExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ using Harmony;
 using Harmony.ILCopying;
 using LbmLib.Language;
 
+// TODO: This is a bad idea - do NOT use. Better to use an updated version of Harmony and take advantage of self-patching.
 namespace LbmLib.Harmony
 {
 	public static class HarmonyExtensions
@@ -132,3 +134,4 @@ namespace LbmLib.Harmony
 		}
 	}
 }
+*/
